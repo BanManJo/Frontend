952abcfab4502689a4634225571d9fc0df0d40ba
Seoungkwang Kim <net0310@gmail.com>
2021-09-12 01:45:08 +0900

    trivial changes.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Config from './config';
 import router from './router';
 import Vuetify from 'vuetify';
 import VueResource from 'vue-resource';
-import { DemoRepository } from './models/DemoRepository';
+import {DemoRepository} from './models/DemoRepository';
 
 let Web3_1 = require('web3');
 
@@ -14,93 +14,90 @@ Vue.config.productionTip = false;
 
 // state management
 let store = {
-    debug: true,
-    state: {
-        // metamask state variable
-        metamask: {
-            web3DefaultAccount: '0xcB51566f355B8D5523bd0DF01498A45352B0D59a',
-            metamaskInstalled: true,
-            networkId: '3',
-        },
-
-        // local web3 instance(not metamask)
-        web3 : null,
-    },
-    networkReady() {
-        return this.getNetworkId() != '' && this.getMetamaskInstalled() && this.getWeb3DefaultAccount() != ''
-    },
-    setNetworkId(networkId) {
-        this.state.metamask.networkId = networkId;
-    },
-    getNetworkId() {
-        return this.state.metamask.networkId;
+  debug: true,
+  state: {
+    // metamask state variable
+    metamask: {
+      web3DefaultAccount: '0xcB51566f355B8D5523bd0DF01498A45352B0D59a',
+      metamaskInstalled: true,
+      networkId: '3',
     },
 
-    setWeb3DefaultAccount(account) {
-        this.state.metamask.web3DefaultAccount = account;
-    },
-    getWeb3DefaultAccount() {
-        return this.state.metamask.web3DefaultAccount;
-    },
-
-    setMetamaskInstalled(){
-        this.state.metamask.metamaskInstalled = true;
-    },
-    getMetamaskInstalled(){
-        return this.state.metamask.metamaskInstalled;
-    },
-
-    getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+    // local web3 instance(not metamask)
+    web3: null,
+  },
+  networkReady() {
+    return this.getNetworkId() != '' && this.getMetamaskInstalled() && this.getWeb3DefaultAccount() != '';
+  },
+  setNetworkId(networkId) {
+    this.state.metamask.networkId = networkId;
+  },
+  getNetworkId() {
+    return this.state.metamask.networkId;
+  },
+  setWeb3DefaultAccount(account) {
+    this.state.metamask.web3DefaultAccount = account;
+  },
+  getWeb3DefaultAccount() {
+    return this.state.metamask.web3DefaultAccount;
+  },
+  setMetamaskInstalled() {
+    this.state.metamask.metamaskInstalled = true;
+  },
+  getMetamaskInstalled() {
+    return this.state.metamask.metamaskInstalled;
+  },
+  getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 };
 
 const getWeb3Instance = () => {
-    const web3 = new Web3_1(window.ethereum);
-    return web3;
-  };
+  const web3 = new Web3_1(window.ethereum);
+  return web3;
+};
 
 const getWeb3 = () => {
-    return new Promise((resolve, reject) => {
-      window.addEventListener("load", async () => {
-        if (window.ethereum) {
-          const web3 = new Web3_1(window.ethereum);
-          try {
-            // ask user permission to access his accounts
-            await window.ethereum.request({ method: "eth_requestAccounts" });
-            resolve(web3);
-          } catch (error) {
-            reject(error);
-          }
-        } else {
-          reject("must install MetaMask");
+  return new Promise((resolve, reject) => {
+    window.addEventListener("load", async () => {
+      if (window.ethereum) {
+        const web3 = new Web3_1(window.ethereum);
+        try {
+          // ask user permission to access his accounts
+          await window.ethereum.request({method: "eth_requestAccounts"});
+          resolve(web3);
+        } catch (error) {
+          reject(error);
         }
-      });
+      } else {
+        reject("must install MetaMask");
+      }
     });
-  };
+  });
+};
 
 Vue.mixin({
-    created: function () {
-        this.$DemoRepoInstance = new DemoRepository();
-        // one instance of web3 available to all components
-        if (typeof web3 !== 'undefined') {
-            web3 = getWeb3Instance();
-            console.log(web3);
-            this.$DemoRepoInstance.setWeb3(web3);
-        }
-        // inject config to components
-        this.$config = Config;
+  created: function () {
+    this.$DemoRepoInstance = new DemoRepository();
+    // one instance of web3 available to all components
+    if (typeof web3 !== 'undefined') {
+      web3 = getWeb3Instance();
+      console.log(web3);
+      this.$DemoRepoInstance.setWeb3(web3);
     }
+    // inject config to components
+    this.$config = Config;
+  }
 });
 
 new Vue({
-    el: '#app',
-    data: {
-        globalState: store,
-    },
-    router,
-    template: '<App/>',
-    components: { App },
-    mounted() {
-    }
+  el: '#app',
+  data: {
+    globalState: store,
+  },
+  router,
+  template: '<App/>',
+  components: {App},
+  mounted() {
+  }
 });
