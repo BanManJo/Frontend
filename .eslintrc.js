c530e5bc8d5d5a86401a57a9288bfa564aba881a
Seoungkwang Kim <net0310@gmail.com>
2021-09-25 19:24:04 +0900

    off the rules of eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     node: true,
   },
   extends: "vuetify",
+  /*
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
@@ -11,6 +12,14 @@ module.exports = {
     "vuetify/no-deprecated-classes": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-dupe-class-members": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
+  */
+  {
+    "rules": {
+        "eqeqeq": "off",
+        "curly": "error",
+        "quotes": ["error", "double"]
+    }
   },
   parserOptions: {
     parser: "babel-eslint",
