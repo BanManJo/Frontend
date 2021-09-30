df33991dc9f118a5f60dd37539ab684674781d02
tjdrn0809 <tjdrn0917234@gmail.com>
2021-09-30 09:29:10 +0900

    contract address

--- src/config.js
+++ src/config.js
@@ -6,13 +6,13 @@ const AdminTestRepository = require("./contracts/AdminTest");
 //대영
 //address:
 // 성구
-//address:
+//address: 0xba9be9d17Bc8FC3097F134Cf4F06ca8810621f0b
 //현수
 //address: 0x494e5437C8D2C1d4D99df401aC180a648242B869
 module.exports = {
-  DEMO_ADDRESS: "0x2585d9a685aCF4F99F8f7d99a3351211cd6a7339",
+  DEMO_ADDRESS: "0x0812375e6a5A8A8a2Fdbea6C9B0F7d05287197dc",
   DEMO_ABI: DemoRepository.abi,
-  ADMIN_TEST_ADDRESS: "0x494e5437C8D2C1d4D99df401aC180a648242B869",
+  ADMIN_TEST_ADDRESS: "0xba9be9d17Bc8FC3097F134Cf4F06ca8810621f0b",
   ADMIN_TEST_ABI: AdminTestRepository.abi,
   GAS_AMOUNT: 500000
 };
