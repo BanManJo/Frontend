5af9877d3bf005098b52a8fc757245ccb0050134
MoSangIl-mac <mosangil-mac@student.com>
2021-10-09 15:22:43 +0900

    Change config

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const ChickenHouseRepository = require("./contracts/ChickenHouse");
 const OrderRoomRepository = require("./contracts/OrderRoom");
 
 //상일
-//Admin: 0x13c97ecd59d2fd674858C7396F900f2761C1A784 / 0x3fB752462A3E1B258065D6ccA050c29dcA4d89A7 (mac / window)
+//Admin: 0x8D451f91442FdaEB86eb9BAfA14a1850498f3149 / 0x3fB752462A3E1B258065D6ccA050c29dcA4d89A7 (mac / window)
 //Demo: 0x13c97ecd59d2fd674858C7396F900f2761C1A784 / 0x0C0989bdC8c053598BCBfeD5c0A1D597f649008A (mac / window)
 //대영
 //address:   0xF1e056e1C62691419F77a592EbeD21B01C4d87Ac
@@ -19,7 +19,7 @@ const OrderRoomRepository = require("./contracts/OrderRoom");
 module.exports = {
   DEMO_ADDRESS: "0x0FB5516C550bfCE1e19cE1F912bCaeB94aAa3fde",
   DEMO_ABI: DemoRepository.abi,
-  ADMIN_ADDRESS: "0x3fB752462A3E1B258065D6ccA050c29dcA4d89A7",
+  ADMIN_ADDRESS: "0x8D451f91442FdaEB86eb9BAfA14a1850498f3149",
   ADMIN_ABI: AdminRepository.abi,
   CHICKEN_HOUSE_ABI: ChickenHouseRepository.abi,
   ORDER_ROOM_ABI: OrderRoomRepository.abi,
