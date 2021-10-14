8a6319432c898dabfd17911b636a1c3fc3cdf305
SangIl-fnino <mosang1230@naver.com>
2021-10-14 09:59:04 +0900

    REmove space in usermypage

--- src/components/OwnerDialog.vue
+++ src/components/OwnerDialog.vue
@@ -1,37 +1,47 @@
 <template>
-  <v-app>
-    <v-container>
-      <!-- modal -->
-      <v-dialog v-model="owner.ownerModal" max-width="290">
-        <v-card>
-          <v-card-title class="headline">메뉴 수정</v-card-title>
-          <v-card-title class="headline">{{ owner.storeName }}</v-card-title>
-          <v-card-text style="text-align: center">
-            <v-progress-circular indeterminate color="red" v-show="owner.isLoading"></v-progress-circular>
-            <v-col style="height: 100%; padding-bottom: 5px" xs12 sm12 md12>
-              <!-- <v-text-field
+  <!-- modal -->
+  <v-dialog v-model="owner.ownerModal" max-width="290">
+    <v-card>
+      <v-card-title class="headline">메뉴 수정</v-card-title>
+      <v-card-title class="headline">{{ owner.storeName }}</v-card-title>
+      <v-card-text style="text-align: center">
+        <v-progress-circular
+          indeterminate
+          color="red"
+          v-show="owner.isLoading"
+        ></v-progress-circular>
+        <v-col style="height: 100%; padding-bottom: 5px" xs12 sm12 md12>
+          <!-- <v-text-field
                 v-model="owner.roomNumber"
                 placeholder="1"
                 label="사진"
                 persistent-hint
               ></v-text-field>-->
-              <v-text-field v-model="menu" placeholder="BBQ" label="메뉴" persistent-hint></v-text-field>
-              <v-checkbox v-model="sunsal" label="순살?"></v-checkbox>
+          <v-text-field
+            v-model="menu"
+            placeholder="BBQ"
+            label="메뉴"
+            persistent-hint
+          ></v-text-field>
+          <v-checkbox v-model="sunsal" label="순살?"></v-checkbox>
 
-              <v-text-field v-model="price" placeholder="Chicken" label="가격" persistent-hint></v-text-field>
-            </v-col>
-            <v-col style="height: 100%; padding-bottom: 5px" xs12 sm12 md12>
-              <v-btn @click="setMenus" color="teal">확인</v-btn>
-            </v-col>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-      <!-- 다이아로그 끝 -->
-    </v-container>
-  </v-app>
+          <v-text-field
+            v-model="price"
+            placeholder="Chicken"
+            label="가격"
+            persistent-hint
+          ></v-text-field>
+        </v-col>
+        <v-col style="height: 100%; padding-bottom: 5px" xs12 sm12 md12>
+          <v-btn @click="setMenus" color="teal">확인</v-btn>
+        </v-col>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+  <!-- 다이아로그 끝 -->
 </template>
 
 <script>
@@ -49,7 +59,7 @@ export default {
       sunsal: false,
 
       AdminInstance: contractInstance.getAdminInstance(), // Admin Instance data,
-      succeed: false,
+      succeed: false
       // not loaded on map page
       // isLoading: false,
     };
@@ -72,8 +82,9 @@ export default {
       const CHAddress = await this.AdminInstance.findChickenHouse(
         this.owner.storeName
       );
-      const ChickenHouseInstance =
-        contractInstance.getChickenHouseInstance(CHAddress);
+      const ChickenHouseInstance = contractInstance.getChickenHouseInstance(
+        CHAddress
+      );
 
       if (this.sunsal == true) {
         await ChickenHouseInstance.setMenu(
@@ -97,11 +108,11 @@ export default {
       // var con_test = confirm("등록하신 메뉴를 수정하시겠습니까?");
       // console.log(menuInfo);
       // this.$emit("menuChanged", menuInfo);
-    },
+    }
     // sendEvent: function() {
     //   this.$emit("update");
     // }
-  },
+  }
 };
 </script>
 
